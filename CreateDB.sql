fcc6845bd9f606c267f29ce86d07dfb438272d27
stefanjepsen <43956167+stefanjepsen@users.noreply.github.com>
2020-09-22 21:29:51 +0200

    Update CreateDB.sql

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,2 +1,26 @@
 DROP DATABASE IF EXISTS EmployeeDB;
-CREATE DATABASE
+CREATE DATABASE EmployeeDB;
+USE EmployeeDB;
+
+CREATE TABLE 'Location' (
+    LocationID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(255)
+);
+
+CREATE TABLE 'PostalCode' (
+    PostalCodeID varchar(20) NOT NULL PRIMARY KEY,
+    City varchar(255)
+);
+
+CREATE TABLE 'Employee' (
+    EmployeeID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    FirstName varchar(255) NULL,
+    Innitials varchar(255) NULL,
+    LastName varchar(255) NULL,
+    Street varchar(255) NULL,
+    PostalCode varchar(20) NOT NULL,
+    Gender varchar(1) NOT NULL,
+    BirthDate DATE NOT NULL,
+    DepartmentID int NOT NULL
+
+);
