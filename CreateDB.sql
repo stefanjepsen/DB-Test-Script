493392af09846cbdf1ead6df0fe85c951e75a021
stefanjepsen <43956167+stefanjepsen@users.noreply.github.com>
2020-09-22 21:45:10 +0200

    Update CreateDB.sql

--- CreateDB.sql
+++ CreateDB.sql
@@ -2,17 +2,18 @@ DROP DATABASE IF EXISTS EmployeeDB;
 CREATE DATABASE EmployeeDB;
 USE EmployeeDB;
 
-CREATE TABLE 'Location' (
-    LocationID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE `Location` (
+    LocationID varchar(20) int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Name varchar(255)
 );
 
-CREATE TABLE 'PostalCode' (
+CREATE TABLE PostalCode (
     PostalCodeID varchar(20) NOT NULL PRIMARY KEY,
     City varchar(255)
 );
 
-CREATE TABLE 'Employee' (
+CREATE TABLE Employee (
     EmployeeID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     FirstName varchar(255) NULL,
     Innitials varchar(255) NULL,
@@ -22,5 +23,32 @@ CREATE TABLE 'Employee' (
     Gender varchar(1) NOT NULL,
     BirthDate DATE NOT NULL,
     DepartmentID int NOT NULL
+
+);
+
+
+CREATE TABLE Department (
+    DepartmentID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    Name varchar(255) NULL,
+    EmployeeID int NOT NULL,
+    LocationID int NOT NULL,
+    LastName varchar(255) NULL
+
+);
+
+CREATE TABLE ProjectID(
+    ProjectID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    Name varchar(255) NULL,
+    DepartmentID int NOT NULL,
+    LocationID int NOT NULL
+    
+
+);
+
+CREATE TABLE WorksOn (
+    EmployeeID int NOT NULL,
+    ProjectID int NOT NULL,
+    Hours float NULL,
+    CONSTRAINT PK_WorksOn PRIMARY KEY (EmployeeID, ProjectID)
 
 );
